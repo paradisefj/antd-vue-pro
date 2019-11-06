4ac28e87a158517395af44b1e91d9a2a59f9c695
fangjuan <fangjuanyuyue@gmail.com>
2019-11-06 12:01:00 +0800

    添加vuex saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexSaga from 'vuex-saga';
+import { call, put } from 'vuex-saga'
+import request from '../utils/request';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+async function login(userInfo) {
+  return request(`http://localhost:8080/api/auth/doLogin`, {
+    method: 'POST',
+    body: userInfo
+  })
+}
+const store = new Vuex.Store({
   state: {
   },
   mutations: {
+    AUTH_USER_LOGIN: (state, { info }) => {
+      state.authedUser = info;
+    }
   },
   actions: {
+  	*login(state, { userInfo }) {
+      const data = yield call(login, userInfo);
+      if(data.authedUser) {
+        yield put({
+          type: 'AUTH_USER_LOGIN',
+          info: data.authedUser
+        })
+      }
+  	}
   },
   modules: {
+    
   },
 });
+
+Vue.use(VuexSaga, { store: store })
+
+export default store;
---
+++ src/views/User/Login.vue
@@ -0,0 +1,57 @@
+<template>
+  <a-form
+      :form="form"
+      @submit="handleSubmit"
+    >
+      <a-form-item
+        label="用户名"
+        :label-col="{ span: 5 }"
+        :wrapper-col="{ span: 12 }"
+      >
+        <a-input
+          v-decorator="[
+            'userName',
+            {rules: [{ required: true, message: 'Please input your note!' }]}
+          ]"
+        />
+      </a-form-item>
+      <a-form-item
+        label="密码"
+        :label-col="{ span: 5 }"
+        :wrapper-col="{ span: 12 }"
+      >
+        <a-input
+          v-decorator="[
+            'password',
+            {rules: [{ required: true, message: 'Please input your note!' }]}
+          ]"
+        />
+      </a-form-item>
+      <a-button @click="handleSubmit">登录</a-button>
+    </a-form>
+
+</template>
+
+<script>
+import Vue from 'vue';
+export default {
+  name: 'home',
+  data() {
+    return {
+      form: this.$form.createForm(this, { name: 'coordinated' }),
+    }
+  },
+  created() {
+    
+  },
+  methods: {
+    handleSubmit(e) {
+      const userInfo = this._data.form.getFieldsValue();
+      console.log(userInfo);
+      //debugger;
+      //this.$store.dispatch('login', userInfo);
+      this.$run("login", { userInfo });
+    }
+  }
+}
+</script>
